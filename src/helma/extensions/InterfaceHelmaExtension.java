a9b5f3592630ce2dd01e327dc736db20b3b22adc
Daniel Ruthardt <daniel@dowee.com>
2010-12-11 02:34:02 +0100

    Abstract keyword was not removed in previous commit.

--- src/helma/extensions/InterfaceHelmaExtension.java
+++ src/helma/extensions/InterfaceHelmaExtension.java
@@ -37,26 +37,26 @@ public interface InterfaceHelmaExtension {
      * called by the Server at startup time. should check wheter the needed classes
      * are present and throw a ConfigurationException if not.
      */
-    public abstract void init(Server server) throws ConfigurationException;
+    public void init(Server server) throws ConfigurationException;
 
     /**
      * called when an Application is started. This should be <b>synchronized</b> when
      * any self-initialization is performed.
      */
-    public abstract void applicationStarted(Application app)
+    public void applicationStarted(Application app)
             throws ConfigurationException;
 
     /**
      * called when an Application is stopped.
      * This should be <b>synchronized</b> when any self-destruction is performed.
      */
-    public abstract void applicationStopped(Application app);
+    public void applicationStopped(Application app);
 
     /**
      * called when an Application's properties are have been updated.
      * note that this will be called at startup once *before* applicationStarted().
      */
-    public abstract void applicationUpdated(Application app);
+    public void applicationUpdated(Application app);
 
     /**
      * called by the ScriptingEngine when it is initizalized. Throws a ConfigurationException
@@ -65,7 +65,7 @@ public interface InterfaceHelmaExtension {
      * with pairs of varname and ESObjects. This method should be <b>synchronized</b>, if it
      * performs any other self-initialization outside the scripting environment.
      */
-    public abstract HashMap initScripting(Application app,
+    public HashMap initScripting(Application app,
             ScriptingEngine engine) throws ConfigurationException;
 
     /**
@@ -73,6 +73,6 @@ public interface InterfaceHelmaExtension {
      *
      * @return ...
      */
-    public abstract String getName();
+    public String getName();
 
 }
